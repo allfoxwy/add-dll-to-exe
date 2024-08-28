e297f52d54211d11fe3d1bf8ca81fbaf0cb55f81
allfoxwy <3644645+allfoxwy@users.noreply.github.com>
2024-08-29 04:10:30 +0800

    Disable ExperimentalWarning

--- add-dll-to-exe.js
+++ add-dll-to-exe.js
@@ -252,6 +252,21 @@ function isPE() {
 }
 
 
+// Disable ExperimentalWarning
+const originalEmit = process.emit;
+process.emit = function (name, data, ...args) {
+  if (
+    name === `warning` &&
+    typeof data === `object` &&
+    data.name === `ExperimentalWarning` 
+    //if you want to only stop certain messages, test for the message here:
+    //&& data.message.includes(`Fetch API`)
+  ) {
+    return false;
+  }
+  return originalEmit.apply(process, arguments);
+};
+
 
 
 (function main() {
---
+++ build-sea.bat
@@ -0,0 +1,14 @@
+
+del  sea-prep.blob
+
+node --experimental-sea-config sea-config.json 
+
+del hello.exe
+
+copy node.exe hello.exe
+
+signtool remove /s hello.exe
+
+
+
+npx postject hello.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
